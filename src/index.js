c108ba56fb05148e78e507ad80147553503b37d4
Mita <milosrancic94@gmail.com>
2020-04-08 16:28:53 +0200

    added browser router and basename prop

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename="/reactjs-gmail-clone">
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
