e2c74e5ef267181279c4f28f8665f2aba70f4444
Mita <milosrancic94@gmail.com>
2020-04-08 16:34:24 +0200

    fixed routes

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,15 +1,31 @@
 import React from "react";
 import "./TopNav.scss";
+import { Switch, Route, Link } from "react-router-dom";
 
 import Social from "./Social/Social";
 import Promotions from "./Promotions/Promotions";
 import Primary from "./Primary/Primary";
 
-const TopNav = (props) => {
+const TopNav = () => {
   return (
     <div className="TopNav">
+      <Link to="/topnav/primary">Primary</Link>
+      <Link to="/topnav/social">Social</Link>
+      <Link to="/topnav/promotions">Promotions</Link>
 
-    </div >
+      <Switch>
+        <Route path="/topnav/primary">
+          <Primary />
+
+        </Route>
+        <Route path="/topnav/social">
+          <Social />
+        </Route>
+        <Route path="/topnav/promotions">
+          <Promotions />
+        </Route>
+      </Switch>
+    </div>
   )
 }
 
