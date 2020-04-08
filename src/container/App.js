be7792802e2bd9f07d9eda52216ccd1a8b24d59e
Mita <milosrancic94@gmail.com>
2020-04-08 16:34:18 +0200

    //

--- src/container/App.js
+++ src/container/App.js
@@ -3,12 +3,25 @@ import './App.scss';
 import SideNav from "../components/SideNav/SideNav";
 import TopNav from "../components/TopNav/TopNav";
 
+import { Switch, Route, Link } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* <SideNav /> */}
-        <TopNav />
+        {/* <TopNav /> */}
+        <Link to="/sidenav">side</Link>
+        <Link to="/topnav">top</Link>
+
+        <Switch>
+          <Route path="/sidenav">
+            <SideNav />
+          </Route>
+          <Route path="/topnav">
+            <TopNav />
+          </Route>
+        </Switch>
       </div>
     )
   }
