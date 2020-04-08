15dbad84a31a947f85e0c208cda5d8536a8b6319
Mita <milosrancic94@gmail.com>
2020-04-08 15:55:29 +0200

    ..

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -8,11 +8,7 @@ import Primary from "./Primary/Primary";
 const TopNav = (props) => {
   return (
     <div className="TopNav">
-      <span style={{ display: "flex" }}>
-        <Primary />
-        <Social />
-        <Promotions />
-      </span>
+
     </div >
   )
 }
