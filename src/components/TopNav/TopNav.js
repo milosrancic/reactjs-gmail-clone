6c3a5194ba7e27beb5fe584f3bf77d7a10708cc9
Mita <milosrancic94@gmail.com>
2020-04-08 15:32:01 +0200

    updated import statements

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,5 +1,20 @@
 import React from "react";
+import "./TopNav.scss";
 
-const TopNav = () => <p>this is TOPNAV component!</p>
+import Social from "./Social/Social";
+import Promotions from "./Promotions/Promotions";
+import Primary from "./Primary/Primary";
+
+const TopNav = (props) => {
+  return (
+    <div className="TopNav">
+      <span style={{ display: "flex" }}>
+        <Primary />
+        <Social />
+        <Promotions />
+      </span>
+    </div >
+  )
+}
 
 export default TopNav;
