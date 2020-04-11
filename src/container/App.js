da83f5a2d9a530d795dd6791e1c3514b12a5bd88
Mita <milosrancic94@gmail.com>
2020-04-11 10:24:42 +0200

    code cleanup

--- src/container/App.js
+++ src/container/App.js
@@ -3,24 +3,11 @@ import './App.scss';
 
 import Layout from "../components/Layout";
 
-import { Switch, Route, Link } from "react-router-dom";
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout />
-        {/* <Link to="/sidenav">side</Link>
-        <Link to="/topnav">top</Link>
-
-        <Switch>
-          <Route path="/sidenav">
-            <SideNav />
-          </Route>
-          <Route path="/topnav">
-            <TopNav />
-          </Route>
-        </Switch> */}
       </div>
     )
   }
