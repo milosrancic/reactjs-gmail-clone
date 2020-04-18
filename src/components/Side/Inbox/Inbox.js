fed9cb2330cd7a4567a971ce97344736e7338f88
Mita <milosrancic94@gmail.com>
2020-04-18 12:24:28 +0200

    /

--- src/components/Side/Inbox/Inbox.js
+++ src/components/Side/Inbox/Inbox.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Hoc from "../../Hoc";
 
-const styleLi = {
-  borderBottom: "1px solid gray"
-}
-
 const Inbox = (props) => {
   return (
     <Hoc>
       <p>INBOX</p>
-      {/* {props.inbox} */}
       {console.log("this is props in inbox", props.state)}
       {props.state.map((item) => 
-      <li style={styleLi} key={item.id}>{item.email}</li>
+      <li key={item.id}>{item.email}</li>
         )}
     </Hoc>
   )
