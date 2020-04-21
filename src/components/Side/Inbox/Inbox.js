7b6510aaf1faf7c8df9a427f11d90171b28ef593
Mita <milosrancic94@gmail.com>
2020-04-21 19:28:25 +0200

    /d

--- src/components/Side/Inbox/Inbox.js
+++ src/components/Side/Inbox/Inbox.js
@@ -4,9 +4,10 @@ import Hoc from "../../Hoc";
 const Inbox = (props) => {
   return (
     <Hoc>
+  {console.log("inbox:", props)}
       <p>INBOX</p>
       {props.state.map((item) => 
-      <li key={item.id} onClick={props.click}>{item.email}</li>
+      <li key={item.id} onClick={() => props.click(item.id)}>{item.email}</li>
         )}
     </Hoc>
   )
--- src/components/Side/Starred/Starred.js
+++ src/components/Side/Starred/Starred.js
@@ -4,9 +4,10 @@ import Hoc from "../../Hoc";
 const Starred = (props) => {
   return (
     <Hoc>
+      {console.log("starred:", props)}
       <p>STARRED</p>
       {props.state.map((item) => 
-        <li key={item.id} onClick={props.click}>{item.email}</li>
+        <li key={item.id} onClick={() => props.click(item.id)}>{item.email}</li>
       )}
     </Hoc>
   )
