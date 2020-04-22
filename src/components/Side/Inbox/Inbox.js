25165b01271025a4007c160ec1417677ed19ee01
Mita <milosrancic94@gmail.com>
2020-04-22 15:40:16 +0200

    update new layout for item component

--- src/components/Side/Inbox/Inbox.js
+++ src/components/Side/Inbox/Inbox.js
@@ -1,40 +1,13 @@
 import React from 'react';
-import Hoc from "../../Hoc";
-
-const styleCheckbox = {
-  background: "orange",
-  transform: "scale(1.3)",
-  verticalAlign:"center"
-}
-
-const styleLi = {
-  padding: "20px",
-}
-const star = {
-  marginLeft: "5px",
-  marginRight: "20px",
-}
-
-const title = {
-  textAlign: "center"
-}
-
+import Item from "../../Item";
 
 const Inbox = (props) => {
   return (
     <>
-    {/* <Hoc> */}
-  {console.log("inbox:", props)}
-      <p style={title}>INBOX</p>
-      {props.state.map((item) => 
-      <li key={item.id} style={styleLi} >
-          <input style={styleCheckbox} type="checkbox"  />
-          <i className="far fa-star" style={star}></i>
-           {item.email}
-        
-      </li>
-        )}
-    {/* </Hoc> */}
+        <Item 
+          title="inbox"
+          state={props.state}
+        />
   </>
   )
 }
