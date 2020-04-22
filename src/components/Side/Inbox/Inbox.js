eba85b9edaba99568f580d6de983cf68a74d3490
Mita <milosrancic94@gmail.com>
2020-04-22 13:50:19 +0200

    added basic style

--- src/components/Side/Inbox/Inbox.js
+++ src/components/Side/Inbox/Inbox.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Hoc from "../../Hoc";
 
+const styleCheckbox = {
+  background: "orange",
+  transform: "scale(1.3)",
+  verticalAlign:"center"
+}
+
+const styleLi = {
+  padding: "20px",
+}
+const star = {
+  marginLeft: "5px",
+  marginRight: "20px",
+}
+
+const title = {
+  textAlign: "center"
+}
+
+
 const Inbox = (props) => {
   return (
     <Hoc>
   {/* {console.log("inbox:", props)} */}
-      <p>INBOX</p>
+      <p style={title}>INBOX</p>
       {props.state.map((item) => 
-      <li key={item.id} >{item.email}</li>
+      <li key={item.id} style={styleLi} >
+          <input style={styleCheckbox} type="checkbox"  />
+          <i className="far fa-star" style={star}></i>
+           {item.email}
+        
+      </li>
         )}
     </Hoc>
   )
