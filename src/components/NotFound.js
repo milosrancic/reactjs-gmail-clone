7dd80c6b8bee484409d164555cd5a1591df94ffd
Mita <milosrancic94@gmail.com>
2020-04-23 19:29:55 +0200

    created default route, not found

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const style = {
+  margin: "100px auto",
+  textAlign: "center",
+  fontSize: "30px"
+}
+
+const NotFound = () => ( <p style={style}>Sorry, but the page you've requested has not been found!</p> )
+
+export default NotFound;
