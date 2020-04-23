caa157f44ee14da5fbfa4dbc36569bb23399000a
Mita <milosrancic94@gmail.com>
2020-04-23 18:58:59 +0200

    added sender to state

--- src/container/App.js
+++ src/container/App.js
@@ -6,33 +6,39 @@ import Layout from "../components/Layout";
 class App extends Component {
   constructor(props) {
     super(props);
-
+  
     this.state = {
       inbox: [
         {
           id: 1,
+          sender: "Jane Doe",
           email: "Lorem ipsum dolor sit amet."
         },
         {
           id: 2,
+          sender: "Josh One",
           email: "Lorem ipsum dolor sit amet, consectetur"
         },
         {
           id: 3,
+          sender: "Mike Mike",
           email: "lorem ipsum dolor something"
         }
       ],
       starred: [
         {
           id: 4,
+          sender: "Lily Mike",
           email: "Lorem ipsum, dolor sit amet consectetur adipisicing."
         },
         {
           id: 5,
+          sender: "Paul Awesome",
           email: "Lorem ipsum dolor sit amet consectetur."
         },
         {
           id: 6,
+          sender: "These Nuts",
           email: "Some awesome email 999"
         }
       ]
