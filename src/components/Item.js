19d5a3a6edf5da85f4cf4375a8a58bc86e1d1037
Mita <milosrancic94@gmail.com>
2020-05-04 14:32:56 +0200

    moved styling to separate file

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,68 +1,34 @@
 import React from "react";
+import "./Item.scss"
 
-const styleDiv = {
-  width: "70%",
-  background: "lightgray",
-  margin: "3rem auto"
-}
-
-const styleCheckbox = {
-  // background: "orange",
-  // transform: "scale(1.3)",
-  verticalAlign:"top"
-}
-
-const styleLi = {
-  padding: "20px",
-}
-const star = {
-  marginLeft: "5px",
-  marginRight: "20px",
-}
-
-const title = {
-  textAlign: "center",
-  textTransform: "uppercase"
-}
-
-const email = {
-  paddingLeft: "30px",
-
-}
-
-const sender = {
-  // paddingRight: "3rem",
-  // width: "100px"
-}
 
 const Item = props => {
-
-  const method = props.addToStarred;
-  console.log('method', method)
-  if (props.title === "inbox") {
-    console.log("inbox")
-    console.log(props.addToStarred)
-  } 
-  if (props.title === "starred") {
-    console.log('starred')
-    console.log(props.addToInbox)
-  }
-
+  console.log("props", {props})
+  // console.log('method', method)
+  // if (props.title === "inbox") {
+    // console.log("inbox")
+    // console.log(props.addToStarred)
+   
   return (
-    <div style={styleDiv}>
-      {console.log({props})}
-    <p style={title}>{props.title}</p>
-    <ul>
-      {props.state.map((item) => 
-      <li key={item.id} style={styleLi} >
-          <input style={styleCheckbox} type="checkbox"  />
-          <i 
-            className="far fa-star" 
-            style={star} 
-            onClick={() => props.addToStarred(item)}></i>
-          <span style={sender}>{item.sender}</span> <span style={email}>{item.email}</span>
-      </li>
-        )}
+    <div className="Item">
+      <p className="title">{props.title}</p>
+
+      <button>mark all</button>
+      {/* mark all mark as read/unread  */}
+
+      <ul>
+        {props.state.map((item) => 
+        <li className="li" key={item.id}>
+            <input type="checkbox"  className="checked" />
+            <i 
+              className="far fa-star" 
+              onClick={() => (console.log("clicked"))}>
+            </i>
+            <span className="sender">{item.sender}</span> 
+            <span className="email">{item.email}</span>
+            <i className="fas fa-trash"></i>
+        </li>
+          )}
       </ul>
     </div>
   )
