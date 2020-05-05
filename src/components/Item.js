b679cdcc37e5bf9b50727501164142398adaeb02
Mita <milosrancic94@gmail.com>
2020-05-05 18:31:15 +0200

    asd

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,12 @@ import "./Item.scss"
 
 const Item = props => {
 
-  console.log("props", {props})
+  // wont work need to be ul > li
+  {props.state.map((item) => {
+    console.log(item.checked);
+    let {id, sender, email, checked} ={item};
+  })}
+  console.log("passed props:", {props})
   // console.log('method', method)
   // if (props.title === "inbox") {
     // console.log("inbox")
@@ -12,18 +17,20 @@ const Item = props => {
    
   return (
     <div className="Item">
+    {console.log("passed props:", {props})}
       <p className="title">{props.title}</p>
 
       <button>mark all</button>
       {/* mark all mark as read/unread  */}
       <ul>
+        {/* onClick={() => props.toggleChecked(item)} */}
         {props.state.map((item) =>
-        <li className={`li`}  key={item.id}>
-            <input type="checkbox"   />
-            <i 
+        <li className={`li`}  key={item.id}  >
+            <input type="checkbox"  />
+            {/* <i 
               className="far fa-star" 
-              onClick={() => (console.log(item.checked))}>
-            </i>
+              onClick={() => (console.log(item))}>
+            </i> */}
             <span className="sender">{item.sender}</span> 
             <span className="email">{item.email}</span>
             <i className="fas fa-trash"></i>
