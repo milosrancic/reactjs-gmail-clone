2ae721cfdba62f85131bab0fa5a8ca9b3f44b480
Mita <milosrancic94@gmail.com>
2020-07-24 16:47:11 +0200

    ,

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Item.scss"
 
 const Item = props => {
-  // console.log("ITEM props:", {props});
+  console.log("ITEM props:", {props});
     return (
     <div className="Item">
       <p className="title">{props.title}</p>
