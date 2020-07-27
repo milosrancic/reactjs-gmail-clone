2665ca6b3e746240693a2e0bc43cf0c3256a91b4
Mita <milosrancic94@gmail.com>
2020-07-27 18:14:50 +0200

    added logic for displaying message if there are no more emails in folder

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./Item.scss"
 
-const Item = props => {
-    return (
+const Item = props => {           
+  return (
     <div className="Item">
       <p className="title">{props.title}</p>
 
-      <button>mark all</button>
+      {/* {
+        props.state.length > 0 
+        ?  <button>mark all</button>
+        :   `There are no messages left in ${props.title}` 
+      } */}
+      
+      
+      {/* move this styling into coresponding css file (item.scss), don't leave it like this! */}
+     <div style={{marginTop: "3em"}}> 
+       {
+          props.state.length < 1 
+          ?  <p style={{textAlign: "center", marginTop: "4em", fontSize:"18px"}}>There are no messages left in <span style={{fontWeight:"bold"}}> {props.title} </span></p>
+          :  <button>mark all</button> 
+        }
+      </div>
+      
       {/* mark all mark as read/unread  */}
+      {/* <button>mark all</button> */}
       <ul>
         {props.state.map(item =>
         <li className="li"  key={item.id}>
