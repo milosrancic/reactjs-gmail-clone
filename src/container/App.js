cc4815231fb4d06af49d7714501972ddedeee28d
Mita <milosrancic94@gmail.com>
2020-11-10 19:06:49 +0100

    ./

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import uuid from 'react-uuid';
-import './App.scss';
+import React, { useState } from "react";
+import uuid from "react-uuid";
+import "./App.scss";
 
 import Layout from "../components/Layout";
 
 const App = () => {
-  
   const [inbox, setInbox] = useState([
     {
       tag: "inbox",
@@ -13,7 +12,7 @@ const App = () => {
       sender: "Jane Doe",
       email: "Lorem ipsum dorom sit amet.",
       isChecked: false,
-      starred: true
+      starred: true,
     },
     {
       tag: "inbox",
@@ -21,7 +20,7 @@ const App = () => {
       sender: "Josh One",
       email: "Lorem ipsum dolor sit amet, consectetur",
       isChecked: false,
-      starred: false 
+      starred: false,
     },
     {
       tag: "inbox",
@@ -29,8 +28,8 @@ const App = () => {
       sender: "Mike Mike",
       email: "random placeholder text",
       isChecked: false,
-      starred: true
-    }
+      starred: true,
+    },
   ]);
 
   const [spam, setSpam] = useState([
@@ -40,7 +39,7 @@ const App = () => {
       sender: "Lily Mike",
       email: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
       isChecked: false,
-      starred: false
+      starred: false,
     },
     {
       tag: "spam",
@@ -48,8 +47,8 @@ const App = () => {
       sender: "As Die",
       email: "some random text in email body",
       isChecked: false,
-      starred: false
-    }
+      starred: false,
+    },
   ]);
 
   const [trash, setTrash] = useState([
@@ -59,88 +58,84 @@ const App = () => {
       sender: "Mike Mikey",
       email: "this email is in trash",
       isChecked: false,
-      starred: false
-    }
+      starred: false,
+    },
   ]);
 
   const [checked, setChecked] = useState(false);
 
-
-  const addToTrash = item => {
+  const addToTrash = (item) => {
     // console.log("add to trash", item);
-    setTrash([item, ...trash ]);
+    setTrash([item, ...trash]);
 
     // CHECK THOSE IF STATEMENTS
     // if email is from inbox
     // if (item.tag === "inbox") {
-      // remove that email from inbox
-      setInbox([...inbox.filter(inbox => inbox !== item)])
+    // remove that email from inbox
+    setInbox([...inbox.filter((inbox) => inbox !== item)]);
     // }
 
     // if email is from spam
     // if (item.tag === "spam") {
-      // remove that email from spam
-      setSpam([...spam.filter(spam => spam !== item)])
+    // remove that email from spam
+    setSpam([...spam.filter((spam) => spam !== item)]);
     // }
-  }
-  
-  const addToInbox = item => {
+  };
+
+  const addToInbox = (item) => {
     // console.log("add to inbox", item)
     // this.setState({
     //   inbox: [
     //     {
     //       ...item
-    //     },        
+    //     },
     //     ...this.state.inbox,
     //   ]
     // })
-  }
+  };
 
   const addToStarred = (item) => {
-    console.log("addToStarred", item)
+    console.log("addToStarred", item);
 
     // console.log("add to starred", item)
     // console.log(item);
-  }
+  };
 
   // const addToSpam = item => {
   //   console.log("addtoSPAM: ", item)
   // }
 
-  
-
-  const toggleChecked = item => {
-    console.log("toggle checked", item)
+  const toggleChecked = (item) => {
+    console.log("toggle checked", item);
     // setInbox([item => !item.checked])
     // console.log(this.state.inbox[item])
-    // console.log("original checked state",   
+    // console.log("original checked state",
     // item.checked, !item.checked);
-    setChecked(checked => !checked);
-  }
+    setChecked((checked) => !checked);
+  };
 
-  const markAll = e => {
+  const markAll = (e) => {
     console.log("mark all button");
     // let itemName = e.target.name;
     // let checked = e.target.checked;
-  }
+    let itemName = e.target.email;
+    let checked = e.target.isChecked;
+  };
 
-    return (
-      <div className="App">
-        <Layout 
-          inbox={inbox}
-          spam={spam}
-          trash={trash}
-          addToInbox={addToInbox}
-          addToTrash={addToTrash}
-          addToStarred={addToStarred}
-          toggleChecked={toggleChecked}
-          markAll={markAll}
+  return (
+    <div className="App">
+      <Layout
+        inbox={inbox}
+        spam={spam}
+        trash={trash}
+        addToInbox={addToInbox}
+        addToTrash={addToTrash}
+        addToStarred={addToStarred}
+        toggleChecked={toggleChecked}
+        markAll={markAll}
       />
-      </div>
-    )
-  }
-  
-
-
+    </div>
+  );
+};
 
 export default App;
