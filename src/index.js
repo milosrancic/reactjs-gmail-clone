e12cc8be66f3c39720e8531e8d4f88e01ef72453
Mita <milosrancic94@gmail.com>
2020-11-16 20:46:41 +0100

    imported bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './container/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./container/App";
 import { BrowserRouter } from "react-router-dom";
 
-const rootNode = document.getElementById('root');
+const rootNode = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter basename="/reactjs-gmail-clone">
     <App />
